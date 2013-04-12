fa2db37622b5551e531b687a563efe1d676e05ea
you_got <okamoto.yutaka@karakurimono.biz>
2013-04-12 19:56:17 +0900

    練習問題２を解きました.

--- 01/okamoto/janken.rb
+++ 01/okamoto/janken.rb
@@ -9,3 +9,27 @@ def atodashi(hand)
      when :paa then :choki
   end
 end
+
+def janken(person1, person2)
+  j1 = poi
+  j2 = poi
+
+  puts "#{person1} is #{j1}"
+  puts "#{person2} is #{j2}"
+
+  case judge(j1, j2)
+     when -1 then "#{person1} won"
+     when 0 then "draw"
+     when 1 then "#{person2} won"
+  end
+end
+
+def judge(janken1, janken2)
+  if janken1 == janken2
+    return 0
+  elsif janken1 == atodashi(janken2)
+    return -1
+  else
+    return 1
+  end
+end
